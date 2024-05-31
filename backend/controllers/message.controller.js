598ee8f4f058fa44a2250a8c1ac30e2e858ab762
Eswar <manikantapadala358@gmail.com>
2024-05-31 09:33:02 +0530

    5/31/2024

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,20 @@
+import Conversation from '../models/conversation.models.js';
+
+
+export const sendMessage = async (req,res)=>{
+    try{
+        const {message} = req.body;
+        const { id : receiverId} = req.params;
+        const senderId = req.user._id
+
+
+        await Conversation.findOne({
+            participants:{$all:[senderId,receiverId]},
+
+        })
+    } catch(error){
+
+        console.log("Error in sendMessage controller:",error.message)
+        res.status(500).json({error:"Internal server error"});
+    }
+};
